3a7d5d70b1cee3992f8cf2346832b0867b4554e9
Ansa2610 <asavilova@gmail.com>
2024-06-18 13:55:51 +0300

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,9 @@
+name = "Anna"
+print('Name:',name)
+age = 44
+print(type(age))
+print('Age:',age)
+print("New age:", age + 10)
+name = 'is student:'
+is_student = 10
+print('is_student:', is_student == 10)
